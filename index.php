7bbc0d4e5d7929c0428c7584590444622e4eb43e
Pauline Dupraz <duprazpauline@gmail.com>
2013-05-25 16:42:11 +0200

    initial commit: created the index

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	
+<link rel="stylesheet/less" type="text/css" href="css/style.less" /> 
+	
+<script type="text/javascript" src="js/lib/jquery.min.js"></script>
+<script type="text/javascript" src="js/lib/less.js"></script>
+
+<script type="text/javascript" src="js/script.js"></script>
+
+<title>title</title>
+</head>
+<body>
+<div class="main">
+	<div class="header">
+	</div>
+		
+	<div class="main-container container_12">
+		
+	</div>
+		
+	<div class="footer">
+	</div>
+</div>
+</body>
+</html>
+
+
+
